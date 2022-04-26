5430130303a1527f414088e496426f91a463335d
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-04-26 14:52:13 +0800

    Update tiktok.js

--- script/tiktok.js
+++ script/tiktok.js
@@ -1,6 +1,6 @@
    let code = 'JP';
    if ($request.url.indexOf("get_domains/v5/") != -1) {
    let url = $request.url.replace(/\=CN/g, "=" + code);
-         $done({ headers: { Location: url }, status: 'HTTP/1.1 302 Found'}) 
+         $done({ headers: { Location: url }, status: 'HTTP/1.1 302'}) 
 
     } 
