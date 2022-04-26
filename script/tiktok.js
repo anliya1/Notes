807946c9fa7d09e36c4ef514e6d2828e82e1b436
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-04-26 14:25:22 +0800

    Update tiktok.js

--- script/tiktok.js
+++ script/tiktok.js
@@ -1,8 +1,25 @@
-let code = 'JP';
- if ($request.url.indexOf("=CN") != -1) {
+
+    if ($request.url.indexOf("get_domains/v5/") != -1) {
+
+        $done({ response: { body: "" } });
+
+    } else if ($request.url.indexOf("aweme/v2/feed/") != -1) {
+
+        let url = $request.url.replace(/https:\/\/.+tiktokv.+aweme\/v2(\/feed\/.+)/, "https://api.tiktokv.com/aweme\/v1$1");
+
+        url = url.replace(/\=CN/g, "=" + code);
+        $done({
+            response: { status: 'HTTP/1.1 307 Temporary Redirect', headers: { Location: url } }
+        });
+
+    } else if ($request.url.indexOf("=CN") != -1) {
+
         let url = $request.url.replace(/\=CN/g, "=" + code);
-        $done({ status: 'HTTP/1.1 307', headers: { Location: url } });
+
+        $done({ response: { status:'HTTP/1.1 307 Temporary Redirect', headers: { Location: url } } });
 
     } else {
-       $done({});
+
+        $done({})
     }
+
