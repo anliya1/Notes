f0b7510b5a0122d9ece8b6c26cc2c5d13694bbd3
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-04-26 13:27:26 +0800

    Update tiktok.js

--- script/tiktok.js
+++ script/tiktok.js
@@ -1,7 +1,7 @@
 let code = 'JP';
  if ($request.url.indexOf("=CN") != -1) {
         let url = $request.url.replace(/\=CN/g, "=" + code);
-        $done({ response: { status: 'HTTP/1.1 307 Temporary Redirect', headers: { Location: url } } });
+        $done({ response: { status: 'HTTP/1.1 307', headers: { Location: url } } });
 
     } else {
        $done({});
