345b0023c2cbef75f1803ae9377309469587324e
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-07-11 17:04:26 +0800

    Update haijiao.js

--- script/haijiao.js
+++ script/haijiao.js
@@ -2,14 +2,6 @@ let body = $response.body;
 let obj = JSON.parse(body);
 obj.data.type= 0;
 
- for(let a in obj.data.attachments){
-    if(obj.data.attachments[a].coverUrl){
-        let id =obj.data.attachments[a].id;
-        let url =obj.data.attachments[a].remoteUrl;
-        let aa ='/'+id+'_i';
-         url = url.replace(/\/\d+_i_preview/, aa);
-        obj.data.attachments[a].remoteUrl =url;
-    }
- }
+
 
 $done({ body: JSON.stringify(obj) });
