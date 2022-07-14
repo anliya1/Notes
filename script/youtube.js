19f71c956d582a8faa463fbf34ddd6351381b4b9
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-07-14 17:44:23 +0800

    Update youtube.js

--- script/youtube.js
+++ script/youtube.js
@@ -1,54 +1,9 @@
-/****** 
-配置
-scopyright = youtube搜索Happy白嫖
-telegram = https://t.me/baipiao_666
-
-#!name=Youtube
-#!desc=跳过视频广告
-#!system=ios-quantumult x
-
-[rewrite_local]
-
-(^https?:\/\/[\w-]+\.googlevideo\.com\/.+)&ctier=L(&.+?),ctier,(.+) url 302 $1$2$3
-^https?:\/\/.+?\.(googlevideo|googleapis)\.com\/.+(player\/ad_break|&oad) url reject
-[MITM]
-hostname = *.googlevideo.com,*.googleapis.com
-
-
-#!name=Youtube
-#!desc=跳过视频广告
-#!system=ios-Loon
-
-[Script]
-
-http-request ^https?:\/\/.+?\.(googlevideo|googleapis)\.com\/.+(player\/ad_break|(&oad|ctier=L)) script-path=https://raw.githubusercontent.com/anliya1/Notes/master/script/youtube.js, requires-body=false, timeout=10, tag=tiktok
-
-[MITM]
-hostname = *.googlevideo.com,*.googleapis.com
-
-
-#!name=Youtube
-#!desc=跳过视频广告
-#!system=ios-Surge or ios-shadawrocket
-#！shadawrocket 删除hostname=后面的 %APPEND%
-[Script]
-
-tiktok=type=http-request,pattern=^https?:\/\/.+?\.(googlevideo|googleapis)\.com\/.+(player\/ad_break|(&oad|ctier=L)),requires-body=0,script-path=https://raw.githubusercontent.com/anliya1/Notes/master/script/youtube.js
-
-[MITM]
-hostname = %APPEND% *.byteoversea.com,*.tiktokv.com
-
-*******/
-
-
-     
-let status = 302;
 if ($request.url.indexOf("&oad") != -1) {
   $done({ response: {body: ""} });
 } else if ($request.url.indexOf("&ctier") != -1) {
   let url = $request.url.replace("&ctier=L", "");
    url = url.replace("ctier", "");
-  $done({ response: { status: status, headers: { Location: url } } });
+  $done({ response: { status: 302, headers: { Location: url } } });
 } else if
 ($request.url.indexOf("player/ad_break") != -1) {
   $done({ response: {body: ""} });
