39654bcf013e6191607028a5bdfb2557b6b6a8e6
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-07-22 14:30:08 +0800

    Update hj.js

--- script/hj.js
+++ script/hj.js
@@ -1,13 +1,13 @@
-
-
 let body = $response.body;
 let obj = JSON.parse(body);
-let id;
-let a ;
+let id ;
  for(let a in obj.data.attachments){
     if(obj.data.attachments[a].coverUrl){
-       id =obj.data.attachments[a].id;
-        let url = `https://hjedd.com/api/topic/att/1987885`+id;
+        id =obj.data.attachments[a].id;
+    }
+ }
+
+let url = `https://hjedd.com/api/topic/att/`+id;
 const method = `GET`;
 const headers = {
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
@@ -25,14 +25,13 @@ const myRequest = {
     headers: headers,
     body: body
 };
+
 $task.fetch(myRequest).then(response => {
-     a = JSON.parse(response.body)
- obj.data.attachments[a].remoteUrl=a.data[0].url
- $done({ body: JSON.stringify(obj) });
-});
+   let o = JSON.parse(response.body);
+    let b =o.data[0].url;
 }, reason => {
     console.log(reason.error);
     $done();
 });
-    }
- }
+obj.data.attachments[a].remoteUrl =b;
+$done({ body: JSON.stringify(obj) });
