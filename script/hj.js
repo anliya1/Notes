dc3387c5c880f2936f28e23c1b41b3190c3883f0
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-07-23 08:41:51 +0800

    Update hj.js

--- script/hj.js
+++ script/hj.js
@@ -6,16 +6,9 @@
 function httpa (id) {
 return new Promise((resolve, reject) =>{
 
-const url = 'https://hjedd.com/api/topic/att/'+id;
-const method = `GET`;
-const headers = {
-'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Host' : `hjedd.com`,
-'Connection' : `keep-alive`,
-'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
-'Accept-Encoding' : `gzip, deflate, br`,
-'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`
-};
+const url = 'https://'+$reqeuest.headers[Host]+'/api/topic/att/'+id;
+const method = $reqeuest.method;
+const headers = $reqeuest.headers;
 const body = ``;
 
 const myRequest = {
