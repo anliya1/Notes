bd5ebce67e12ae8e80150a1ac6c74df02dc128aa
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-07-23 16:50:49 +0800

    Update hj.js

--- script/hj.js
+++ script/hj.js
@@ -9,8 +9,7 @@ return new Promise((resolve, reject) =>{
 let url = 'https://'+$request.headers.Host+'/api/topic/att/'+id;
 let method = $request.method;
 let headers = $request.headers;
-const body = ``;
-
+const body = '';
 const myRequest = {
     url: url,
     method: method,
@@ -19,18 +18,12 @@ const myRequest = {
 };
 $task.fetch(myRequest).then(response => {
 	resolve(response.body);
-   
 }, reason => {
     console.log(reason.error);
 	reject(reason.error);
 });
 })};
-
-// https://hjedd.com/api/topic/att/1987885
-
- async function haijiao(body){
-  
- // let oo = await httpa('https://hjedd.com/api/topic/380729');
+async function haijiao(body){
   let obj1 = JSON.parse(body);
   for(let a in obj1.data.attachments){
     if(obj1.data.attachments[a].coverUrl){
@@ -38,10 +31,9 @@ $task.fetch(myRequest).then(response => {
         let o = await httpa(id);
 	   let obj2 = JSON.parse(o);
            obj1.data.attachments[a].remoteUrl =obj2.data[0].url;
-	   // console.log(obj1.data.attachments[a].remoteUrl);
-	   $done({ body: JSON.stringify(obj1) });
-    }
+     }
  }
+   $done({ body: JSON.stringify(obj1)});
 };
 
 haijiao($response.body);
