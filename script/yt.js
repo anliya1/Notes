6a35708c97b6d58e93485e66256075e6710430b6
Happy白嫖 <64358456+anliya1@users.noreply.github.com>
2022-11-07 17:31:51 +0800

    Update yt.js

--- script/yt.js
+++ script/yt.js
@@ -1,6 +1,6 @@
 const isQuanX = typeof $task !== "undefined";
-if ($request.url.indexOf("&ctier") != -1) {
-  let url = $request.url.replace("&ctier=L", "");
+if ($response.Location.indexOf("&ctier") != -1) {
+  let url = $response.Location.replace("&ctier=L", "");
    url = url.replace("ctier", "");
-  $done({{status: 'HTTP/1.1 302 Found',headers: { Location: url}});
+  $done({status: 'HTTP/1.1 302 Found',Location: url});
 }
